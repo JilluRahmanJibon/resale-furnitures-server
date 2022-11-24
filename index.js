6f9d2056cbbce9e193d6b9c0f32a5f57c5ba37f6
Jillu Rahman Jibon <jillurahmanjibon@gmail.com>
2022-11-24 14:14:32 +0600

    mongodb are added

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("review server is running now ");
+});
+app.listen(port, () => {
+	console.log("port is running", port);
+});
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.jf2skzr.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+
+
+async function run() {
+
+}
+run().catch(err => {
+	console.log(err);
+});
